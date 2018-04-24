a063c9532b68bccf2f3876f72093eda2ec1ee14f
snkoski <shawnnkoski@gmail.com>
2018-04-24 16:04:00 -0400

    add find character movies functionality

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,7 +6,28 @@ def get_character_movies_from_api(character)
   #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
+  found_character = character_hash["results"].find { |character_info|
+  character_info["name"].downcase == character.downcase}
+  if found_character == nil
+    return nil
+  else
+    found_character["films"].collect do |movie|
+       RestClient.get(movie)
+     end
+  end
+
+  #BAD STUFF WE DID EARLIER
+  #movies = nil
+  # character_hash["results"].each do |character_info|
+  #   if character_info["name"].downcase == character.downcase
+  #     movies = character_info["films"]
+  #   end
+  # end
+  # movies.collect do |movie|
+  #   RestClient.get(movie)
+  # end
+
+
   # iterate over the character hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -20,12 +41,22 @@ end
 
 def parse_character_movies(films_hash)
   # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts JSON.parse(film)["title"]
+  end
 end
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  if films_hash == nil
+    puts "Character not found"
+    return
+  else
+    parse_character_movies(films_hash)
+  end
 end
+
+#show_character_movies("luke skywalker")
 
 ## BONUS
 
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,16 @@
 def welcome
   # puts out a welcome message here!
+  puts "May the force be with you?"
 end
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  gets.chomp
+end
+
+def get_character_attribute_from_user
+  puts "What would you like to know?"
+  puts "\n Height, Eye Color, Films, Species"
+  gets.chomp
 end
